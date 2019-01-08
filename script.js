fde338c83692d4a1e46815be13ab811f350d3802
liu <liu@seraku.co.jp>
2019-01-08 11:47:23 +0900

    updating responsive design part

--- script.js
+++ script.js
@@ -5,5 +5,17 @@ $(function(){
         var arrow = $(".nav-container .hover-arrow ." + className);
         arrow.toggleClass("hidden-arrow");
     });
+
+    $(".login img.open-button").click(function(){
+        $(this).hide();
+        $(".login img.close-button").show();
+        $("nav").show();
+    });
+
+    $(".login img.close-button").click(function(){
+        $(this).hide();
+        $(".login img.open-button").show();
+        $("nav").hide();
+    });
     
 });
